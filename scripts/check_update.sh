0bef5adbaad988fbe76d8db1353fccf43258c892
Filouz <mp180506@gmail.com>
2026-01-19 19:35:30 +0100

    - fix

--- scripts/check_update.sh
+++ scripts/check_update.sh
@@ -5,8 +5,9 @@
 #
 
 LOCAL_VERSION_FILE="/etc/portal_auth_version"
-REMOTE_VERSION_URL="https://raw.githubusercontent.com/FilouzMC/portal-auth-tls/main/version.txt"
-REMOTE_INSTALL_URL="https://raw.githubusercontent.com/FilouzMC/portal-auth-tls/main/install.sh"
+REMOTE_VERSION_URL="https://raw.githubusercontent.com/FilouzMC/portal-auth-tls/$GITHUB_BRANCH/version.txt"
+REMOTE_INSTALL_URL="https://raw.githubusercontent.com/FilouzMC/portal-auth-tls/$GITHUB_BRANCH/install.sh"
+GITHUB_BRANCH="speed-project-with-updater"
 TMP_INSTALLER="/tmp/portal_auth_install.sh"
 
 log() {
